cd8b5333a14abce506ef381ef947e7c333dd8468
bhavanan-agi <92708070+bhavanan-agi@users.noreply.github.com>
2023-08-30 23:28:55 +0530

    corrected

--- streamlitapp.py
+++ streamlitapp.py
@@ -19,9 +19,9 @@ fruits_to_show=x.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 
