2a9717372ab059a16cde711a04640e2a788c447f
bhavanan-agi <92708070+bhavanan-agi@users.noreply.github.com>
2023-08-30 23:37:33 +0530

    updated code

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,9 +1,7 @@
 import streamlit
-import pandas
-
-
-
-
+import pandas as pd  # Corrected pandas import
+import requests
+import snowflake.connector
 
 print("Hello")
 streamlit.title("My parent's new diner")
@@ -12,27 +10,20 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-x=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+x = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 x = x.set_index('Fruit')
-fruits_selected=streamlit.multiselect("Pick some fruits:",list(x.index),['Avocado','Strawberries'])
-fruits_to_show=x.loc[fruits_selected]
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(x.index), ['Avocado', 'Strawberries'])
+fruits_to_show = x.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-
-fruityvice_normalized=pandas.json_normalized(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())  # Corrected pandas function name
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import snowflake.connector
-
-
-
-
+# Snowflake code here (if properly formatted)
