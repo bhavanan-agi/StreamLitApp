fd765282e097c41541ebaa5fafd804a82e01d370
bhavanan-agi <92708070+bhavanan-agi@users.noreply.github.com>
2023-08-30 15:27:56 +0530

    set the index

--- streamlitapp.py
+++ streamlitapp.py
@@ -10,6 +10,7 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 x=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
+x = x.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:",list(x.index))
 
 streamlit.dataframe(x)
